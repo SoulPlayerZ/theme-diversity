05802323503e68493293165e58b2c7e1b8364361
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-01-28 19:41:30 -0300

    refactor: Apply css on the select with bulma

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import "../css/header.css"
+
 
 function Header () {
   return (
     <header>
-      <select>
-        <option>option1</option>
-        <option>option2</option>
-        <option>option3</option>
-      </select>
+      <label htmlFor="theme" class="select is-link">
+        <select id="theme">
+          <option>option1</option>
+          <option>option2</option>
+          <option>option3</option>
+        </select>
+      </label>
     </header>
 )
 }
