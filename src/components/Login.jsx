a5a14314cf9f1a1adb65dac0bb69051aba826a96
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-01-28 19:26:30 -0300

    refactor: Apply css on the form with bulma

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,11 +5,15 @@ function Login () {
   return (
     <form>
       <h2>Test Login Diversity </h2>
-      <label for="login">Login
-        <input type="text" name="login" placeholder="Digite Login" ></input>
+      <label htmlFor="login">Login
+        <input type="text" class="input is-link" id="login" placeholder="your Login" />
       </label>
+      <label htmlFor="password">password
+        <input type="text" class="input is-link" id="password" placeholder="Your password" />
+      </label>
+      <button class="button is-link">Login</button>
     </form>
-)
+  )
 }
 
 export default Login;
