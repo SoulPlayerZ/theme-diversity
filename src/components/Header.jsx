080bfc4cdec6f8c1ededde1ddf540d2efd4097a6
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-01-28 18:30:49 -0300

    feat: Create Header component in components directory

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header () {
+  return (
+    <header>
+      <select>
+        <option>option1</option>
+        <option>option2</option>
+        <option>option3</option>
+      </select>
+    </header>
+)
+}
+
+export default Header;
+
