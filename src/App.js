143ecce244df84cbf896ac0b64fc273cf87bd25a
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-01-29 13:08:27 -0300

    feat: Insert BrowserRouter and pages with LoginPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import Header from './components/Header';
-import Login from './components/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 import './css/myReset.css';
 
 function App() {
   return (
-    <section>
-      <Header />
-      <Login />
-    </section>
+    <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            {/* <Route path="/chars" element={<Chars />} />
+            {/* <Route path="/locations" element={<Locations />} />
+            <Route path="/episodes" element={<Episodes />} /> */} 
+          </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "../components/Login";
+import Header from "../components/Header";
+import '../css/login.css';
+
+function LoginPage () {
+  return (
+    <section>
+        <Header />
+        <Login />
+    </section>
+  )
+}
+
+export default LoginPage;
