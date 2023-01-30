aa437e0a0b190f96cbd7235a031f8b5a1b0019c6
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-01-30 20:26:55 -0300

    feat: Create ThemeContext

---
+++ src/context/ThemeProvider.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import ThemeContext from "./ThemeContext";
+
+function ThemeProvider ({ children }) {
+  const [theme, setTheme] = useState([]);
+
+const contextValue = {
+    theme,
+    setTheme
+};
+
+return (
+<ThemeContext.Provider value={contextValue}>
+    {children}
+</ThemeContext.Provider>
+);
+}
+
+export default ThemeProvider;
