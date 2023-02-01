917b4d172ff1402f04d1efa43fed86409f14b1d8
SoulPlayerZ <thyrso.kof@gmail.com>
2023-02-01 18:36:54 -0300

    feat: Add more genres

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,11 @@ function Header () {
         <select id="theme" onChange={changeTheme} value={ localStorage.getItem('theme') || 'default' }>
           <option value="default">Default</option>
           <option value="trans">Trans</option>
+          <option value="gay">Gay</option>
           <option value="bi">Bi</option>
+          <option value="pan">Pan</option>
+          <option value="non-binary">Non Binary</option>
+          <option value="fluid">Fluid</option>
         </select>
       </label>
     </header>
